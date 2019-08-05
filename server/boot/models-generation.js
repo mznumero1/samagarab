54246956256f6a2cd114051c03b3625e196dc0fc
mzerbo <mzerbo98@gmail.com>
2019-08-05 11:24:21 +0000

    servver 1.0

---
+++ server/boot/models-generation.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(app) {
+  /*app.dataSources.mysql.autoupdate('Gerant', function(err) {
+    if (err) throw err;
+  });
+  app.dataSources.mysql.autoupdate('Medicament', function(err) {
+    if (err) throw err;
+  });
+  app.dataSources.mysql.autoupdate('Stock', function(err) {
+    if (err) throw err;
+  });
+  app.dataSources.mysql.autoupdate('Pharmacie', function(err) {
+    if (err) throw err;
+  });
+  app.dataSources.mysql.autoupdate('AccessToken', function(err) {
+    if (err) throw err;
+  });*/
+}
